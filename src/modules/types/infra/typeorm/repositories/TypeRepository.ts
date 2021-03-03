66f32b9c08933d6c2bf0eb001982e336dbf7d8ff
Daniel Toloto <daniel.toloto@brainweb.com.br>
2021-03-03 17:10:31 -0300

    add: TypeRepository

---
+++ src/modules/types/infra/typeorm/repositories/TypeRepository.ts
@@ -0,0 +1,38 @@
+import { getRepository, Repository } from 'typeorm';
+
+import ITypeRepository from '@modules/types/repositories/ITypeRepository';
+import ICreateTypeDTO from '@modules/types/dtos/ICreateTypeDTO';
+
+import Type from '../entities/Type';
+
+class TypeRespository implements ITypeRepository {
+  private ormRepository: Repository<Type>;
+
+  constructor() {
+    this.ormRepository = getRepository(Type);
+  }
+
+  public async findById(id: string): Promise<Type | undefined> {
+    const type = await this.ormRepository.findOne({ where: { id } });
+    return type;
+  }
+
+  public async findAll(): Promise<Type[]> {
+    const types = await this.ormRepository.find();
+    return types;
+  }
+
+  public async create(typeData: ICreateTypeDTO): Promise<Type> {
+    const type = this.ormRepository.create(typeData);
+
+    await this.ormRepository.save(type);
+
+    return type;
+  }
+
+  public async save(type: Type): Promise<Type> {
+    return this.ormRepository.save(type);
+  }
+}
+
+export default TypeRespository;
