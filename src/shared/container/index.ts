2d9414d6bb049b07cfbfc6ac55cfc9ecd9d80128
Daniel Toloto <daniel.toloto@brainweb.com.br>
2021-03-03 17:13:23 -0300

    add: register singleton TypeRepository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,1 +1,6 @@
 import { container } from 'tsyringe';
+
+import ITypeRepository from '@modules/types/repositories/ITypeRepository';
+import TypeRepository from '@modules/types/infra/typeorm/repositories/TypeRepository';
+
+container.registerSingleton<ITypeRepository>('TypeRepository', TypeRepository);
