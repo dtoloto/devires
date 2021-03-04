762e667a77db9605db84546791aca800017957bb
Daniel Toloto <daniel.toloto@brainweb.com.br>
2021-03-03 22:57:20 -0300

    fix: CreateUserService tests

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -1,4 +1,5 @@
 import FakeTypeRepository from '@modules/types/repositories/fakes/FakeTypeRepository';
+import AppError from '@shared/errors/AppError';
 import FakeUserRepository from '../repositories/fakes/FakeUserRepository';
 import CreateUserService from './CreateUserService';
 
@@ -10,16 +11,34 @@ describe('CreateUser', () => {
   beforeEach(() => {
     fakeTypeRepository = new FakeTypeRepository();
     fakeUserRepository = new FakeUserRepository();
-    createUserService = new CreateUserService(fakeUserRepository);
+    createUserService = new CreateUserService(
+      fakeUserRepository,
+      fakeTypeRepository,
+    );
   });
 
-  it('should be able to create a new user', async () => {
+  it('should be able to create a new user if current user is root', async () => {
+    const rootType = await fakeTypeRepository.create({
+      name: 'root',
+      description: 'Description Type Root',
+    });
+
+    const root = await fakeUserRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@email.com',
+      password: '123456789',
+      status: true,
+      type_id: rootType.id,
+    });
+
     const type = await fakeTypeRepository.create({
       name: 'Type One',
       description: 'Description Type One',
     });
 
     const { user } = await createUserService.execute({
+      currentUserId: root.id,
+      currentUserTypeId: rootType.id,
       name: 'John Doe',
       email: 'johndoe@email.com',
       password: '123456789',
@@ -28,5 +47,96 @@ describe('CreateUser', () => {
     });
 
     expect(user).toHaveProperty('id');
+  });
+
+  it('should be able to create a new user if current user is admin', async () => {
+    const adminType = await fakeTypeRepository.create({
+      name: 'admin',
+      description: 'Description Type Admin',
+    });
+
+    const admin = await fakeUserRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@email.com',
+      password: '123456789',
+      status: true,
+      type_id: adminType.id,
+    });
+
+    const type = await fakeTypeRepository.create({
+      name: 'Type One',
+      description: 'Description Type One',
+    });
+
+    const { user } = await createUserService.execute({
+      currentUserId: admin.id,
+      currentUserTypeId: adminType.id,
+      name: 'John Doe',
+      email: 'johndoe@email.com',
+      password: '123456789',
+      status: true,
+      type_id: type.id,
+    });
+
+    expect(user).toHaveProperty('id');
+  });
+
+  it('should not be able to create a new user if current user is not root or admin', async () => {
+    const generalType = await fakeTypeRepository.create({
+      name: 'general',
+      description: 'Description Type General',
+    });
+
+    const general = await fakeUserRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@email.com',
+      password: '123456789',
+      status: true,
+      type_id: generalType.id,
+    });
+
+    const type = await fakeTypeRepository.create({
+      name: 'Type One',
+      description: 'Description Type One',
+    });
+
+    await expect(
+      createUserService.execute({
+        currentUserId: general.id,
+        currentUserTypeId: generalType.id,
+        name: 'John Doe',
+        email: 'johndoe@email.com',
+        password: '123456789',
+        status: true,
+        type_id: type.id,
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to create a new user with invalid type id', async () => {
+    const rootType = await fakeTypeRepository.create({
+      name: 'root',
+      description: 'Description Type Root',
+    });
+
+    const root = await fakeUserRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@email.com',
+      password: '123456789',
+      status: true,
+      type_id: rootType.id,
+    });
+
+    await expect(
+      createUserService.execute({
+        currentUserId: root.id,
+        currentUserTypeId: rootType.id,
+        name: 'John Doe',
+        email: 'johndoe@email.com',
+        password: '123456789',
+        status: true,
+        type_id: 'invalid type id',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,10 +1,13 @@
 import { injectable, inject } from 'tsyringe';
 import AppError from '@shared/errors/AppError';
+import ITypeRepository from '@modules/types/repositories/ITypeRepository';
 import User from '../infra/typeorm/entities/User';
 
 import IUserRepository from '../repositories/IUserRepository';
 
 interface IRequest {
+  currentUserId: string;
+  currentUserTypeId: string;
   name: string;
   password: string;
   type_id: string;
@@ -21,15 +24,44 @@ class CreateTypeService {
   constructor(
     @inject('UserRepository')
     private userRepository: IUserRepository,
+
+    @inject('TypeRepository')
+    private typeRepository: ITypeRepository,
   ) {}
 
   public async execute({
+    currentUserId,
+    currentUserTypeId,
     name,
     password,
     type_id,
     email,
     status,
   }: IRequest): Promise<IResponse> {
+    const currentUser = await this.userRepository.findById(currentUserId);
+
+    if (!currentUser) {
+      throw new AppError('Invalid current user id');
+    }
+
+    const currentUserType = await this.typeRepository.findById(
+      currentUserTypeId,
+    );
+
+    if (
+      currentUserType &&
+      currentUserType.name !== 'root' &&
+      currentUserType.name !== 'admin'
+    ) {
+      throw new AppError('Current user is not root/admin', 401);
+    }
+
+    const typeExists = await this.typeRepository.findById(type_id);
+
+    if (!typeExists) {
+      throw new AppError('Invalid type id');
+    }
+
     const user = await this.userRepository.create({
       name,
       password,
