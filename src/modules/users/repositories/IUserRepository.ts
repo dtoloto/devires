7da0d43172af4861c71f0e270b9745fafdb9c100
Daniel Toloto <daniel.toloto@brainweb.com.br>
2021-03-03 21:49:29 -0300

    add: FakeUserRepository

--- src/modules/users/repositories/IUserRepository.ts
+++ src/modules/users/repositories/IUserRepository.ts
@@ -1,6 +1,6 @@
 import { DeleteResult } from 'typeorm';
 import User from '../infra/typeorm/entities/User';
-import ICreateUserDTO from '../dtos/ICreteUserDTO';
+import ICreateUserDTO from '../dtos/ICreateUserDTO';
 
 export default interface IUserRepository {
   findAll(): Promise<User[]>;
