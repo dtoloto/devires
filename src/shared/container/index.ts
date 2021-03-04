8d7a401fb23dbb6923fff0a44be27579261ce6fc
Daniel Toloto <daniel.toloto@brainweb.com.br>
2021-03-04 00:33:21 -0300

    add: HashProvider

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,5 +1,7 @@
 import { container } from 'tsyringe';
 
+import '@modules/users/providers';
+
 import ITypeRepository from '@modules/types/repositories/ITypeRepository';
 import TypeRepository from '@modules/types/infra/typeorm/repositories/TypeRepository';
 
