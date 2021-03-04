e95042f6090f9e7aef43675a07241d7afcfa9d7e
Daniel Toloto <daniel.toloto@brainweb.com.br>
2021-03-04 00:49:40 -0300

    add: UserRepository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -5,4 +5,8 @@ import '@modules/users/providers';
 import ITypeRepository from '@modules/types/repositories/ITypeRepository';
 import TypeRepository from '@modules/types/infra/typeorm/repositories/TypeRepository';
 
+import IUserRepository from '@modules/users/repositories/IUserRepository';
+import UserRepository from '@modules/users/infra/typeorm/repositories/UserRepository';
+
 container.registerSingleton<ITypeRepository>('TypeRepository', TypeRepository);
+container.registerSingleton<IUserRepository>('UserRepository', UserRepository);
