3fdaec76a321c3015ebe8190c510eda25ef67439
Daniel Toloto <daniel.toloto@brainweb.com.br>
2021-03-03 21:44:46 -0300

    add: IUserRepository

---
+++ src/modules/users/repositories/IUserRepository.ts
@@ -0,0 +1,11 @@
+import { DeleteResult } from 'typeorm';
+import User from '../infra/typeorm/entities/User';
+import ICreateUserDTO from '../dtos/ICreteUserDTO';
+
+export default interface IUserRepository {
+  findAll(): Promise<User[]>;
+  findById(id: string): Promise<User | undefined>;
+  create(data: ICreateUserDTO): Promise<User>;
+  save(user: User): Promise<User>;
+  delete(id: string): Promise<DeleteResult | string>;
+}
